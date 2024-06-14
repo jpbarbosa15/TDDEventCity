a4de8de49c27927602fadc21a41617db541c821d
Joao Pedro <jaopedrobarbosa15@gmail.com>
2024-06-14 12:22:43 -0400

    City Test

--- src/main/java/com/devsuperior/demo/controllers/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/devsuperior/demo/controllers/exceptions/ResourceExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.devsuperior.demo.controllers.exceptions;
 
 
+import com.devsuperior.demo.service.exceptions.DataBaseException;
 import com.devsuperior.demo.service.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -13,7 +14,7 @@ import java.time.Instant;
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
-    @ExceptionHandler
+    @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request){
         StandardError err = new StandardError();
         err.setTimestamp(Instant.now());
@@ -23,4 +24,16 @@ public class ResourceExceptionHandler {
         err.setPath(request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
     }
+
+    @ExceptionHandler(DataBaseException.class)
+    public ResponseEntity<StandardError> database(DataBaseException e, HttpServletRequest request){
+        StandardError err = new StandardError();
+        err.setTimestamp(Instant.now());
+        err.setStatus(HttpStatus.BAD_REQUEST.value());
+        err.setError("Database exception");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+    }
+
 }
